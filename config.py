a501b1420fdd881bd1936eb5b8da52541376ee99
Jaylon Nichols <92622379+JaylonNichols@users.noreply.github.com>
2023-10-26 11:16:38 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # Password for TV alerts
-password = "o3K8p7zqAbAy?&Fo"
+password = "test"
 
 #Bybit API info
-BYBIT_TEST_API_KEY = "agJRcXC2ycrMfchux9"
-BYBIT_TEST_SECRET = "l0CFpv0H65vpHeU9NSxA9hfXBLYNRmmPQjyq"
+BYBIT_TEST_API_KEY = "test"
+BYBIT_TEST_SECRET = "test"
 
-BYBIT_API_KEY = "TCzxy14F52PZrYndW5"
-BYBIT_SECERET = "5fy09tGPdc0YXJ05p1dQnnDHRbDvtSn7tleK"
+BYBIT_API_KEY = "test"
+BYBIT_SECERET = "test"
 
-telegram_auth_token = "2031005847:AAGAs94VzLbg9msKGCun79VxR0xTKcybLug"
-telegram_group_id = "BTC_Trade_Notifer"
+telegram_auth_token = "test"
+telegram_group_id = "test"
